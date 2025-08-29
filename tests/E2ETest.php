d9db2008017b6211b1a4ee021d8b549c2065d3ef
Pablo Largo Mohedano <devnix.code@gmail.com>
2025-08-29 18:02:56 +0200

    Add Panther to support e2e tests for live components/turbo

--- tests/E2ETest.php
+++ tests/E2ETest.php
@@ -9,59 +9,32 @@
 
 namespace Symfonycasts\DynamicForms\Tests;
 
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Panther\PantherTestCase;
 use Symfonycasts\DynamicForms\Tests\fixtures\DynamicFormsTestKernel;
 use Zenstruck\Browser\Test\HasBrowser;
 
-class FunctionalTest extends KernelTestCase
+class E2ETest extends PantherTestCase
 {
     use HasBrowser;
 
-    public function testDynamicFields()
+    public function testRecursiveDynamicFields()
     {
-        $browser = $this->browser();
-        $browser->visit('/form')
+        $browser = $this->pantherBrowser();
+        $browser->visit('/form-pizza-selected')
             // check for the hidden field
+            ->waitUntilSeeIn('//html', 'Is Form Valid: no')
             ->assertSeeElement('#test_dynamic_form___dynamic_error')
-            ->assertSee('Is Form Valid: no')
-            // Breakfast is the pre-selected meal
-            ->assertSee('What is for Breakfast?')
-            ->assertContains('<option value="bacon">')
-            ->assertNotContains('<option value="pizza">')
-            ->assertNotContains('What size pizza?')
-        ;
-
-        // change the meal to dinner
-        $browser->selectFieldOption('Meal', 'Dinner')
-            ->click('Submit Form')
-            // changing the field doesn't cause any issues
-            ->assertSee('Is Form Valid: yes')
+            ->assertSee('Pizza 🍕')
             ->assertNotContains('<option value="bacon">')
-            ->assertContains('<option value="pizza">')
-            ->assertNotContains('What size pizza?')
-        ;
-
-        // now select Pizza!
-        $browser->selectFieldOption('Main food', 'Pizza')
-            ->click('Submit Form')
-            ->assertSee('Is Form Valid: yes')
             ->assertContains('<option value="pizza" selected="selected">')
             ->assertContains('What size pizza?')
-        ;
-
-        // select the size
-        $browser->selectFieldOption('Pizza size', '14 inch')
-            ->click('Submit Form')
-            ->assertSee('Is Form Valid: yes')
-            ->assertContains('<option value="pizza" selected="selected">')
-            ->assertContains('<option value="14" selected="selected">')
         ;
 
         // now change the meal to breakfast
         $browser->selectFieldOption('Meal', 'Breakfast')
             ->click('Submit Form')
             // form is not valid: the mainFood submitted an invalid value
-            ->assertSee('Is Form Valid: no')
+            ->waitUntilSeeIn('//html', 'Is Form Valid: no')
             ->assertContains('<option value="bacon">')
             ->assertNotContains('<option value="pizza"')
             ->assertNotContains('What size pizza?')
@@ -71,14 +44,14 @@ class FunctionalTest extends KernelTestCase
         $browser->selectFieldOption('Main food', 'Bacon')
             ->click('Submit Form')
             // form is valid again
-            ->assertSee('Is Form Valid: yes')
+            ->waitUntilSeeIn('//html', 'Is Form Valid: yes')
         ;
 
         // change the meal again
         $browser->selectFieldOption('Meal', 'Lunch')
             ->click('Submit Form')
             // form is not valid: the mainFood=bacon is invalid for lunch
-            ->assertSee('Is Form Valid: no')
+            ->waitUntilSeeIn('//html', 'Is Form Valid: no')
         ;
     }
 
--- tests/fixtures/public/index.php
+++ tests/fixtures/public/index.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 use Symfony\Component\HttpFoundation\Request;
 use Symfonycasts\DynamicForms\Tests\fixtures\DynamicFormsTestKernel;
 
-require __DIR__ . '/../../vendor/autoload.php';
+require dirname(__DIR__, 3).'/vendor/autoload.php';
 
-$kernel = new DynamicFormsTestKernel('test', true);
+$kernel = new DynamicFormsTestKernel($_SERVER['APP_ENV'] ?? 'dev', true);
 $kernel->boot();
 
 $request = Request::createFromGlobals();
